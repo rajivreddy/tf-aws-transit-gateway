90d18f819a6b89ed0b00e5fd037426baddac93a9
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2020-01-07 12:26:49 +0530

    Adding variables

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
   profile                 = var.profile
 }
 
-resource "aws_ec2_transit_gateway" "example" {
+resource "aws_ec2_transit_gateway" "this" {
   description = var.description
   dns_support = var.dns_support
   amazon_side_asn = var.amazon_side_asn
@@ -13,4 +13,16 @@ resource "aws_ec2_transit_gateway" "example" {
   default_route_table_association = var.default_route_table_association
   default_route_table_propagation = var.default_route_table_propagation
   vpn_ecmp_support = var.vpn_ecmp_support
+  tags = merge(
+    {
+      "Name" = var.name
+    },
+    var.additional_tags
+  )
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes = [
+      tags,
+    ]
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "shared_credentials_file" {
+  type    = string
+  default = "/Users/username/.aws/credentials"
+}
+
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "additional_tags" {
+  type = map(string)
+  default = {
+    "createdby" = "devops"
+  }
+}
+
+######### Transit Gateway #########
+variable "name" {
+  type        = string
+  description = "Name of the EC2 Transit Gateway"
+  default     = null
+}
+
+variable "description" {
+  type        = string
+  description = "Description of the EC2 Transit Gateway"
+  default     = null
+}
+
+variable "dns_support" {
+  type        = string
+  description = "Whether DNS support is enabled. Valid values: disable, enable"
+  default     = "enable"
+}
+
+variable "amazon_side_asn" {
+  type        = number
+  description = "Private Autonomous System Number (ASN) for the Amazon side of a BGP session. The range is 64512 to 65534"
+  default     = "64512"
+}
+variable "auto_accept_shared_attachments" {
+  type        = string
+  description = "Whether resource attachment requests are automatically accepted. Valid values: disable, enable"
+  default     = "enable"
+}
+variable "default_route_table_association" {
+  type        = string
+  description = "Whether resource attachments are automatically associated with the default association route table. Valid values: disable, enable"
+  default     = "enable"
+}
+variable "default_route_table_propagation" {
+  type        = string
+  description = "Whether resource attachments automatically propagate routes to the default propagation route table. Valid values: disable, enable"
+  default     = "enable"
+}
+
+variable "vpn_ecmp_support" {
+  type        = string
+  description = "Whether VPN Equal Cost Multipath Protocol support is enabled. Valid values: disable, enable"
+  default     = "enable"
+}
